c32f45570e83d4e4c8e5c1c463933d96bfd8c7c2
ValeraMeza <valeriamezamacias05@gmail.com>
2026-02-02 02:33:32 -0500

    Intento 4

--- djangocrud/wsgi.py
+++ djangocrud/wsgi.py
@@ -5,11 +5,16 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-# Cambiamos 'django_portfolio.settings' por 'djangocrud.settings' 
-# para que coincida con el nombre de tu carpeta actual.
+# Añadimos el directorio base al path para que Python encuentre los módulos sin problemas
+# Esto evita el error de "ModuleNotFoundError" durante el despliegue en Render
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if BASE_DIR not in sys.path:
+    sys.path.append(BASE_DIR)
+
+# Establecemos el módulo de configuración por defecto
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangocrud.settings')
 
 application = get_wsgi_application()
